b21178c708540a07995bb01c95aacb0126271336
Richelle Yirenkyi <richelleyiren@gmail.com>
2022-05-07 18:32:41 +0000

    fetched images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "../styles/form.css"
+import Data from "../data"
 
 export default function Form() {
+    const [url, setUrl] = useState('')
+
+   const getUrl = ()=>{
+       setUrl(memeUrl)
+   }
+
+   const getMeme = Data.data.memes
+   const randomMeme = Math.floor(Math.random() * getMeme.length)
+   const memeUrl = getMeme[randomMeme].url
+
     return (
       <div className="card">
           <h3> Send a meme, have a laugh</h3>
@@ -9,8 +20,9 @@ export default function Form() {
 
           <input type="text"></input>
           <input type="text"></input>
-          <button> Get Meme </button>
+          <button onClick={getUrl}> Get Meme </button>
         </div>
+         <img src={url} className="image"></img>
       </div>
     );
 }
